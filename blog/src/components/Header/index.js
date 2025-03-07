dadd5e925e32ccaa53fd1cedfe2c4338fcda2bb7
rafaelprisco23 <rafaprisco23@gmail.com>
2025-03-07 16:31:12 -0300

    estilizando o menu

--- blog/src/components/Header/index.js
+++ blog/src/components/Header/index.js
@@ -1,14 +1,28 @@
 
+import logosvg from '../../svg/01.svg';
 
-function Header(){
+const Header = () =>{
     
-    let contador = 1;
+    const descLogo = "essa é a logo do blog";
     
     return (
         
+        <>
+            <header className="flex-space-between">
+                <div className="logo">
+                        <img src={logosvg} alt={descLogo}/>
+                </div>
+                <div classNaame="search">
+                <input type="text" className="input-search" name="search" />
+                </div>
+                <ul classNaame="menu">
+                    <li><a href="#"classNaame="nav-link">Home</a></li>
+                    <li><a href="#"classNaame="nav-link">Sobre</a></li>
+                    <li><a href="#"classNaame="nav-link">Contato</a></li>
+                </ul>
+            </header>
         
-        <h1>Header.js {contador}</h1>
-        
+        </>
     );
 }
 
